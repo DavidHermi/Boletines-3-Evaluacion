050564f14468345c6f3b9676dafd098f4761716e
DavidHermi <91197896+DavidHermi@users.noreply.github.com>
2022-04-04 10:02:34 +0200

    Update Main.java
    Commit final main

--- Bolein27/src/com/Boletin27/Main.java
+++ Bolein27/src/com/Boletin27/Main.java
@@ -3,6 +3,8 @@ package com.Boletin27;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	 metodosInterfaz obxMetodos = new metodosInterfaz();
+
+        obxMetodos.crearcompoñentes();
     }
 }
